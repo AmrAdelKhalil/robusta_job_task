391f0142ee3a2fb9ceef1256ab9c8084c72435eb
Amr Adel <amradelkhalil@gmail.com>
2017-10-27 11:00:57 +0200

    build association between models

--- db/migrate/20171027082249_create_directors.rb
+++ db/migrate/20171027082249_create_directors.rb
@@ -1,7 +1,8 @@
 class CreateDirectors < ActiveRecord::Migration[5.1]
   def change
     create_table :directors do |t|
-
+      t.string :name
+      t.references :movie
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+if Rails.env.development?
+  Movie.create!(name: "The mask")
+  Actor.create!(name: "Jim Carry", movie_id: 1)
+  Director.create!(name: "Jack", movie_id: 1)
+  Genre.create!(name: "Comedy", movie_id: 1)
+end
