b7fb1fff8339e4edaa187b83b95ca67a1606803f
Amr Adel <amradelkhalil@gmail.com>
2017-10-28 11:52:55 +0200

    add static page controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root 'movies#index'
+  # root 'movies#index'
+  root 'static_pages#index'
 end
